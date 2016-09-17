5357d47999ae49945c7a391de41d2fbc5beef33d
Qobiljon <qobiljon.toshnazarov@gmail.com>
2016-09-17 15:53:53 +0500

    Design changed! in activity_login.xml

--- app/src/main/java/com/inha/qobiljon/eatnchat/Classes.java
+++ app/src/main/java/com/inha/qobiljon/eatnchat/Classes.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,6 +32,98 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+
+class Credentials {
+    private static SQLiteDatabase db;
+    private static boolean initialized = false;
+    private static Context context;
+
+    private static String username = "username";
+    private static String password = "password";
+
+
+    public static void init(Context context) throws SQLiteException {
+        if (initialized)
+            return;
+
+        Credentials.context = context;
+        Credentials.db = context.openOrCreateDatabase("UserSettings.db", MainActivity.MODE_PRIVATE, null);
+
+        db.execSQL("create table if not exists settings(name varchar(200) primary key, value varchar(200));");
+
+        initialized = true;
+    }
+
+    public static void close() {
+        db.close();
+        db = null;
+    }
+
+
+    public static void setUsername(String newUsername) {
+        if (usernameExists())
+            db.execSQL(String.format("update settings set value='%s' where name='%s'", newUsername, username));
+        else
+            save(username, newUsername);
+    }
+
+    public static void setPassword(String newPassword) {
+        if (passwordExists())
+            db.execSQL(String.format("update settings set value='%s' where name='%s'", newPassword, password));
+        else
+            save(password, newPassword);
+    }
+
+
+    public static String getUsername() {
+        return get(username);
+    }
+
+    public static String getPassword() {
+        return get(password);
+    }
+
+
+    public static void removeUsername() {
+        if (usernameExists())
+            remove(username);
+    }
+
+    public static void removePassword() {
+        if (passwordExists())
+            remove(password);
+    }
+
+
+    public static boolean usernameExists() {
+        return get(username) != null;
+    }
+
+    public static boolean passwordExists() {
+        return get(password) != null;
+    }
+
+
+    private static void save(String name, String value) {
+        db.execSQL("insert into settings(name, value) values('" + name + "', '" + value + "')");
+    }
+
+    private static void remove(String name) {
+        db.execSQL(String.format("delete from settings where name='%s'", name));
+    }
+
+    private static String get(String name) {
+        String ret;
+        Cursor cursor = db.rawQuery("select value from settings where name='" + name + "'", new String[]{});
+
+        if (cursor.moveToFirst())
+            ret = cursor.getString(cursor.getColumnIndex("value"));
+        else ret = null;
+        cursor.close();
+
+        return ret;
+    }
+}
 
 class Lang {
     public static void setLang(String language, Context context) throws NullPointerException {
